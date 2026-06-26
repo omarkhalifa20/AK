a83c08ed41610a07b2e72a845573a178ce0d021e
omarkhalifa20 <omaromarb414@gmail.com>
2026-06-26 17:12:00 +0200

    l

--- src/components/Home/HomeComp.tsx
+++ src/components/Home/HomeComp.tsx
@@ -41,7 +41,7 @@ export default function HomeComp() {
               animate={{ scale: 1, opacity: 1 }}
               transition={{ duration: 1.7, ease: "easeIn" }}
              className=' w-[375px]  md:w-[65%]  lg:w-[52%] xl:w-[42%] 2xl:w-[35%] z-[30] relative top-[6%]  '  src="/homemodel.png" alt="" />
-                <div className='Orbitron flex flex-col h-full  font-bold absolute top-[-25] md:top-[0] lg:top-[150] '>
+                <div className='Orbitron flex flex-col h-full  font-bold absolute top-[-40] md:top-[0] lg:top-[150] '>
                   <motion.h1
                   initial={{ x:-150, opacity: 0 }}
                   animate={{ x:0, opacity: 1 }}
