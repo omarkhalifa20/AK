f05cadb06128b20650232eafd0dffd72f800c925
omarkhalifa20 <omaromarb414@gmail.com>
2026-06-27 02:35:17 +0200

    d

--- src/components/Categories/CategoriesHome.tsx
+++ src/components/Categories/CategoriesHome.tsx
@@ -6,50 +6,50 @@ export default function CategoriesHome() {
   return (
     <>
     <div id='categories' className='container mx-auto pt-8 pb-5 w-[90%]'>
-     <h3 className='text-center mb-20 Bungee text-[35px]'>Our Categories</h3>
+     <h3 className='text-center mb-5 lg:mb-20 Bungee text-[35px]'>Our Categories</h3>
         <div className='grid grid-cols-12 gap-7 overflow-hidden   '> 
           
-          <Link className='col-span-8'  href='/womens'>
+          <Link className='col-span-12 lg:col-span-8'  href='/womens'>
           <motion.div
             
             initial={{ x:-150 , opacity: 0 }} 
             whileInView={{ x:0, opacity: 1 }}
             transition={{ duration: 2, ease: "easeIn"}}
             viewport={{ once: true }}
-           className='  cursor-pointer px-4 flex items-end h-100 rounded-3xl grimg-1 '>
-           <p className='Bungee z-[50] text-[#ffffff] text-[50px]'>Womens  Clothes</p>
+           className='  cursor-pointer px-4 flex justify-center lg:justify-start items-end h-50 lg:h-100 rounded-3xl grimg-1 '>
+           <p className='Bungee z-[50] text-[#ffffff]  text-[33px] lg:text-[50px]'>Womens  Clothes</p>
           </motion.div></Link>
 
-          <Link className='col-span-4'  href='/childs'>
+          <Link className='col-span-12 lg:col-span-4'  href='/childs'>
           <motion.div
            initial={{ x:150 , opacity: 0 }} 
            whileInView={{ x:0, opacity: 1 }}
            transition={{ duration: 2, ease: "easeIn"}}
            viewport={{ once: true }}
-           className=' flex cursor-pointer items-end justify-center h-100 rounded-3xl grimg-2 '>
-          <p className='Bungee z-[50] text-[#007c8f] text-[40px]'> Child Clothes</p>
+           className=' flex cursor-pointer items-end justify-center h-50 lg:h-100 rounded-3xl grimg-2 '>
+          <p className='Bungee z-[50] text-[#007c8f] text-[33px] lg:text-[40px]'> Child Clothes</p>
           </motion.div>
           </Link>
           
-          <Link className='col-span-4'  href='/olds'>
+          <Link className='col-span-12 lg:col-span-4'  href='/olds'>
           <motion.div
            initial={{ x:-150 , opacity: 0 }} 
            whileInView={{ x:0, opacity: 1 }}
            transition={{ duration: 2, ease: "easeIn"}}
            viewport={{ once: true }}
-           className=' flex cursor-pointer items-end justify-center h-100 rounded-3xl grimg-3 '>
-          <p className='Bungee z-[50] text-[#007c8f]  text-[40px]'> Old Man Clothes</p>
+           className=' flex cursor-pointer items-end justify-center h-50 lg:h-100 rounded-3xl grimg-3 '>
+          <p className='Bungee z-[50] text-[#007c8f]  text-[33px] lg:text-[40px]'> Old Man Clothes</p>
           </motion.div>
           </Link>
 
-          <Link className='col-span-8'  href='/mens'>
+          <Link className='col-span-12 lg:col-span-8'  href='/mens'>
           <motion.div 
            initial={{ x:150 , opacity: 0 }} 
            whileInView={{ x:0, opacity: 1 }}
            transition={{ duration: 2, ease: "easeIn"}}
            viewport={{ once: true }}
-          className='col-span-8 h-100 cursor-pointer px-4 flex items-end rounded-3xl grimg-4'>
-          <p className='Bungee z-[50] text-[#ffffff] text-[50px]'>Mens Clothes</p>
+          className='col-span-8 h-50 lg:h-100 cursor-pointer px-4 justify-center lg:justify-start flex items-end rounded-3xl grimg-4'>
+          <p className='Bungee z-[50] text-[#ffffff]  text-[33px] lg:text-[50px]'>Mens Clothes</p>
           </motion.div>
           </Link>
 
